0ff0a638902fa521457ea4744f6948be58630dd5
Bryce Nordgren <bnordgren@gmail.com>
2012-06-30 00:24:50 +0200

    Altered geoserver package to just download the warfile from sourceforge

--- pkgs/servers/http/geoserver/builder.sh
+++ pkgs/servers/http/geoserver/builder.sh
@@ -4,13 +4,15 @@ export LANG="en_US.UTF-8"
 export LOCALE_ARCHIVE=$glibcLocales/lib/locale/locale-archive
 export M2_REPO=$TMPDIR/repository
 
-mkdir -p $TMPDIR
-cd $TMPDIR
-unzip $src
-cd geoserver-2.1.4
-mvn -Dmaven.repo.local=$M2_REPO clean install assembly:attached > $TMPDIR/build.log
+#mkdir -p $TMPDIR
+#cd $TMPDIR
+#unzip $src
+#cd geoserver-2.1.4
+#mvn -Dmaven.repo.local=$M2_REPO clean install assembly:attached > $TMPDIR/build.log
 
 mkdir -p $out
-cp -r $TMPDIR/repository $out
-cp -r $TMPDIR/geoserver-2.1.4/target $out
+cd $out
+unzip $src
+#cp -r $TMPDIR/repository $out
+#cp -r $TMPDIR/geoserver-2.1.4/target $out
 
--- pkgs/servers/http/geoserver/default.nix
+++ pkgs/servers/http/geoserver/default.nix
@@ -1,16 +1,17 @@
-{stdenv, fetchurl, unzip, jdk, ant, maven2 }:
+{stdenv, fetchurl, unzip }:
 
 stdenv.mkDerivation {
   version = "2.1.4" ;
-  name = "geonetwork-2.1.4" ;
-  buildInputs = [ unzip jdk ant maven2] ; 
-#  warfile = "repository/org/geonetwork-opensource/geonetwork-main/2.6.4/geonetwork-main-2.6.4.war" ;
+  name = "geoserver-2.1.4" ;
+  description = "GeoServer is a Java-based software server that allows users to view and edit geospatial data" ;
+  buildInputs = [ unzip ] ; 
+  warfile = "geoserver.war" ;
 
   builder = ./builder.sh ; 
 
   src = fetchurl {
-    url = "mirror://sourceforge/geoserver/2.1.4/geoserver-2.1.4-src.zip" ; 
-    sha256 = "0bh6db1dwsyqxg57wn058jrmi2vpdpk276fyd8ssslk794m22ydy" ; 
+    url = "mirror://sourceforge/geoserver/2.1.4/geoserver-2.1.4-war.zip" ; 
+    sha256 = "10xb7d6l6w8prl1kf2nbyfn3s267gzr74al2gq3b75165civz22j" ;
   };
 
 
