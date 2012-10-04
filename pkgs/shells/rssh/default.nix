2aa7abc938eff1c88262c0b8b4681b3246a68713
Bryce Nordgren <bnordgren@gmail.com>
2012-10-04 19:00:07 +0200

    applied debian's patch which allows rssh to work with rsync protocol 3.0

--- pkgs/shells/rssh/default.nix
+++ pkgs/shells/rssh/default.nix
@@ -7,17 +7,33 @@ assert (supportRsync) -> (rsync != null) ;
 stdenv.mkDerivation rec {
   name = "rssh-2.3.3";
 
-  patches = [] ++ stdenv.lib.optional supportRsync patchSrc ;
+  patches = [] ++ stdenv.lib.optional supportRsync patchSrcs ;
 
   src = fetchurl {
     url = "mirror://sourceforge/rssh/rssh/2.3.3/rssh-2.3.3.tar.gz";
     sha256 = "0zyhdzx04468gaxxw1cqm390gipb5ayhdpk18hg57y454hn92h0r";
   };
 
-  patchSrc =  fetchurl {
-    url =  http://patch-tracker.debian.org/patch/series/dl/rssh/2.3.3-5/fixes/command-line-checking.diff;
-    sha256 = "1bv7dbhspqd3fxljhzp3qfzhmasrwlhyrchx8irvmpqhnjfrldbr";
-  };
+  #
+  # rssh is known to reject rsync protocol 3.0 due to the fact that 
+  # protocol 3.0 overloads the -e option, which could allow the execution
+  # of arbitrary commands.
+  # 
+  # This is the minimum set of patches necessary to cleanly apply debian's 
+  # solution to this problem. Upstream has declared that rssh is in maintainence
+  # mode: only security fixes will make it into releases...and releases will
+  # be few and far between.
+  #
+  patchSrcs = [ 
+    (fetchurl {
+      url =  http://patch-tracker.debian.org/patch/series/dl/rssh/2.3.3-5/fixes/command-line-checking.diff;
+      sha256 = "1bv7dbhspqd3fxljhzp3qfzhmasrwlhyrchx8irvmpqhnjfrldbr";
+    })
+    (fetchurl{
+      url =  http://patch-tracker.debian.org/patch/series/dl/rssh/2.3.3-5/fixes/rsync-protocol.diff;
+      sha256 = "0v37v66pszkkviaarr7n6h2x4k69a45gpsl1dmcq3p8lc3xs5mb8";
+    })
+  ];
 
 
 
