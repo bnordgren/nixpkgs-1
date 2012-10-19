6b11bee43e9ff56f4704706c39039e1a7765100e
Bryce Nordgren <bnordgren@gmail.com>
2012-10-19 10:09:46 -0600

    Updated to drupal 7.16

--- pkgs/servers/http/rxdrupal/default.nix
+++ pkgs/servers/http/rxdrupal/default.nix
@@ -1,9 +1,9 @@
 {stdenv, php}:
 
 stdenv.mkDerivation {
-  version = "7.15" ;
-  name = "rxdrupal-7.15" ;
-  description = "A preconfigured Drupal 7.15 website set up for the RxCadre project." ;
+  version = "7.16" ;
+  name = "rxdrupal-7.16" ;
+  description = "A preconfigured Drupal 7.16 website set up for the RxCadre project." ;
 
   settings= ./settings.php-template ;
   htaccess= ./htaccess-template;
